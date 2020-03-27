c0182037dab88318567fc94b5b3f9fb1af239b0a
chris <morriscp1@cardiff.ac.uk>
2020-03-27 19:30:15 +0000

    can cleary see the result of feed-forward computation withis simplified example on 1 training data sample and how it propagates to the output layer using linear algebra/matrix-vector multiplication

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ rand_weights_1 = np.random.rand(3,4)
 rand_weights_2 = np.random.rand(4, 1)
 print(x.shape)
 print(x)
+
+print("random weights to layer 1")
 print(rand_weights_1)
+
+print("random weights to layer 2 (output layer)")
 print(rand_weights_2)
 
 results = np.dot(x, rand_weights_1)
 
-print("layer 1 neuron values")
+print("layer 1 neuron values before applying activation function (sigma)")
 print(results)
 print("adsf")
 
@@ -24,9 +28,12 @@ def sigmoid(t):
 
 
 layer_1 = sigmoid(results)
+print("layer 1 neurons after applying sigma function to each neuron to squishify - "
+      "value between 0 and 1")
 print(layer_1)
 
+
 layer_2 = sigmoid(np.dot(layer_1, rand_weights_2))
-print("output")
+print("layer 2 output value: after applying sigma")
 print(layer_2)
 
