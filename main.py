2026df5486b45a35eaa9bf40da5bc0dfe594055f
chris <morriscp1@cardiff.ac.uk>
2020-03-30 00:16:39 +0100

    added cost function for y actual and y predicted

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ import numpy as np
 x=np.array(([1,1,1]), dtype=float)
 print(x.shape)
 
+
 rand_weights_1 = np.random.rand(3,4)
 rand_weights_2 = np.random.rand(4, 1)
 print(x.shape)
 print(x)
 
+
 print("random weights to layer 1")
 print(rand_weights_1)
+
 
 print("random weights to layer 2 (output layer)")
 print(rand_weights_2)
 
+
 results = np.dot(x, rand_weights_1)
+
 
 print("layer 1 neuron values before applying activation function (sigma)")
 print(results)
 print("adsf")
+
 
 # activation function
 def sigmoid(t):
@@ -36,4 +42,14 @@ print(layer_1)
 layer_2 = sigmoid(np.dot(layer_1, rand_weights_2))
 print("layer 2 output value: after applying sigma")
 print(layer_2)
+
+
+def cost_function(y_predicted, y_actual):
+    # square difference to make a positive (absolute) value
+    return (y_actual - y_predicted)**2  # power of 2 = squared
+
+
+
+
+
 
